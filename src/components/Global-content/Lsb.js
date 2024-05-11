0f1ce3714581cdf17577864e3a01063fdaef8570
Jashpal Rohit <jassirohit02@gmail.com>
2024-05-11 10:37:35 +0530

    File

--- src/components/Global-content/Lsb.js
+++ src/components/Global-content/Lsb.js
@@ -31,8 +31,8 @@ export default function Lsb() {
                 </li>
                 
             </ul>
-            <a href="./assets/cv/Jashpal-Rohit__CV.pdf" target="_blank" title='Jashpal Rohit - Download CV' className="theme-btn">
-                <i className="las la-download"></i> DOWNLOAD CVV!
+            <a href="./assets/cv/Jashpal-Rohit__CV.pdf" target="_blank" title='Jashpal Rohit - Download CVCC' className="theme-btn">
+                <i className="las la-download"></i> DOWNLOAD CV!
             </a>
         </div>
   )
